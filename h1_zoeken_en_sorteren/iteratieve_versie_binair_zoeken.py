76678391e9518d0b6262bd6ba873ebf0eae04d5d
Warre Van Lancker <warre.vanlancker@student.hogent.be>
2025-10-16 17:14:50 +0200

    Oefeningen in de les

--- h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
+++ h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
@@ -1,2 +1,13 @@
-def zoek_binair(zoekItem,rij):
-    pass
+def zoek_binair(zoekItem, rij):
+    l = 0; r = len(rij) - 1
+    while l != r:
+        m = (l + r) // 2
+        print(f"{l}, {r}")
+        if rij[m] < zoekItem:
+            l = m + 1
+        else:
+            r = m
+    if rij[l] == zoekItem:
+        return l
+    else:
+        return -1
--- h1_zoeken_en_sorteren/sorteren_door_selectie.py
+++ h1_zoeken_en_sorteren/sorteren_door_selectie.py
@@ -1,7 +1,16 @@
 def selection_sort_vooraan(a):
-    pass
+    n = len(a)
+    for i in range(n-1):
+        pos = i
+        min = a[i]
+        for j in range(i + 1, n):
+            if a[j] < min:
+                pos = j
+                min = a[j]
+        a[pos] = a[i]
+        a[i] = min
+        print(a)
 
 if __name__ == "__main__":
     a = [int(_) for _ in input().split()]
     selection_sort_vooraan(a)
-
