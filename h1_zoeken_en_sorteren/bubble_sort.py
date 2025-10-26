ef68fb88ddb46c0d59ab4cf440fdf6dbc4348be5
Warre Van Lancker <warre.vanlancker@student.hogent.be>
2025-10-26 11:36:48 +0100

    Update bubble_sort.py

--- h1_zoeken_en_sorteren/bubble_sort.py
+++ h1_zoeken_en_sorteren/bubble_sort.py
@@ -1,5 +1,13 @@
 def bubble_sort(a):
-    pass
+    n = len(a)
+    uitgevoerd = 0
+    for i in range(n - 1):
+        for j in range(n - 1, i, -1):
+            uitgevoerd += 1
+            if (a[j - 1] > a[j]):
+                a[j - 1], a[j] = a[j], a[j - 1]
+        print(a)
+    print(f"Voor een rij van lengte {n} werd het if-statement {uitgevoerd} keer uitgevoerd")
 
 if __name__ == "__main__":
     a = [int(_) for _ in input().split()]
